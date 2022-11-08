5498e581fe581df361631ed1b6fbc379e9fafa72
1bit-jsf-dev <jfs@1bit.com.ph>
2022-11-08 21:05:02 +0800

    update mobile responsive vendor ui

--- vendor/src/components/Account/Navigation/NavigationContainer.tsx
+++ vendor/src/components/Account/Navigation/NavigationContainer.tsx
@@ -3,13 +3,15 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import styles from "./NavigationContainer.module.scss";
 import Navigation from "./Navigation";
+import NavigationMobileContent from "./NavigationMobileContent";
 
 interface ContainerProps {}
 
 const NavigationContainer: React.FC<ContainerProps> = ({}) => {
   return (
-    <Container fluid="xxl" className="d-none d-lg-block">
+    <Container fluid="xxl">
       <Navigation />
+      <NavigationMobileContent />
     </Container>
   );
 };
