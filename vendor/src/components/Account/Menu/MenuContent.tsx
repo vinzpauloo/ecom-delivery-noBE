5e06ab6f268ee03db2ce9b6539bef00309ff50ed
1bit-jsf-dev <jfs@1bit.com.ph>
2022-11-10 20:56:31 +0800

    update vendor ui

--- vendor/src/components/Account/Menu/MenuContent.tsx
+++ vendor/src/components/Account/Menu/MenuContent.tsx
@@ -1,12 +1,245 @@
 import React, { useState } from "react";
-import { Table, Form, Row, Col, Button } from "react-bootstrap";
+import {
+  Table,
+  Form,
+  Row,
+  Col,
+  Button,
+  Modal,
+  Dropdown,
+} from "react-bootstrap";
 
 import styles from "./MenuContent.module.scss";
 import SearchIcon from "../../../assets/images/search.png";
+import ResetPassword from "../../../pages/Account/ResetPassword";
 
 interface ContainerProps {}
 
+function MenuModal(props: any) {
+  return (
+    <Modal {...props} size="lg">
+      <Modal.Header closeButton className={styles.modalHeader}>
+        <Modal.Title id="contained-modal-title-vcenter">Menu</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className={styles.modalBody}>
+        <Form>
+          <Row>
+            <Col>
+              <h6>Add Menu</h6>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} xs={8}>
+              <Form.Group className="position-relative">
+                <Form.Label>Food Name</Form.Label>
+                <Form.Control
+                  className={styles.inputForm}
+                  type="text"
+                  placeholder="Chinese Special Ramen"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="position-relative">
+                <Form.Label>Food Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} xs={8}>
+              <Form.Group className="position-relative">
+                <Form.Label>Price in PH-PESO</Form.Label>
+                <Form.Control
+                  className={styles.inputForm}
+                  type="text"
+                  placeholder="Php 130.00"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} xs={8}>
+              <Form.Label>Category</Form.Label>
+              <Dropdown>
+                <Dropdown.Toggle className={styles.btnCategory}>
+                  Category
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#action-1">Category1</Dropdown.Item>
+                  <Dropdown.Item href="#action-2">Category2</Dropdown.Item>
+                  <Dropdown.Item href="#action-3">Category3</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col>
+              <Button className={styles.btnUpload}>Upload</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} xs={8}>
+              <Form.Label>Cuisine</Form.Label>
+              <Dropdown>
+                <Dropdown.Toggle className={styles.btnCuisine}>
+                  Cuisine
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#action-1">Cuisine1</Dropdown.Item>
+                  <Dropdown.Item href="#action-2">Cuisine2</Dropdown.Item>
+                  <Dropdown.Item href="#action-3">Cuisine3</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col>
+              <Form.Label>Availability</Form.Label>
+              <Form.Check type="switch" />
+            </Col>
+          </Row>
+          <Row>
+            <Col className="d-flex justify-content-center gap-2">
+              <Button className={styles.btnDiscard} onClick={props.onHide}>
+                Discard
+              </Button>
+              <Button className={styles.btnAddMenu}>Add Menu</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+function EditModal(props: any) {
+  return (
+    <Modal {...props} size="lg">
+      <Modal.Header closeButton className={styles.modalHeader}>
+        <Modal.Title id="contained-modal-title-vcenter">Menu</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className={styles.modalBody}>
+        <Form>
+          <Row>
+            <Col>
+              <h6>Add Menu</h6>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} xs={8}>
+              <Form.Group className="position-relative">
+                <Form.Label>Food Name</Form.Label>
+                <Form.Control
+                  className={styles.inputForm}
+                  type="text"
+                  placeholder="Chinese Special Ramen"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="position-relative">
+                <Form.Label>Food Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} xs={8}>
+              <Form.Group className="position-relative">
+                <Form.Label>Price in PH-PESO</Form.Label>
+                <Form.Control
+                  className={styles.inputForm}
+                  type="text"
+                  placeholder="Php 130.00"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} xs={8}>
+              <Form.Label>Category</Form.Label>
+              <Dropdown>
+                <Dropdown.Toggle className={styles.btnCategory}>
+                  Category
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#action-1">Category1</Dropdown.Item>
+                  <Dropdown.Item href="#action-2">Category2</Dropdown.Item>
+                  <Dropdown.Item href="#action-3">Category3</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col>
+              <Button className={styles.btnUpload}>Upload</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={4} xs={8}>
+              <Form.Label>Cuisine</Form.Label>
+              <Dropdown>
+                <Dropdown.Toggle className={styles.btnCuisine}>
+                  Cuisine
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#action-1">Cuisine1</Dropdown.Item>
+                  <Dropdown.Item href="#action-2">Cuisine2</Dropdown.Item>
+                  <Dropdown.Item href="#action-3">Cuisine3</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col>
+              <Form.Label>Availability</Form.Label>
+              <Form.Check type="switch" />
+            </Col>
+          </Row>
+          <Row>
+            <Col className="d-flex justify-content-center gap-2">
+              <Button className={styles.btnDiscard} onClick={props.onHide}>
+                Discard
+              </Button>
+              <Button className={styles.btnSaveMenu}>Save Menu</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+function DeleteModal(props: any) {
+  return (
+    <Modal {...props} size="lg">
+      <Modal.Header closeButton className={styles.modalHeader}></Modal.Header>
+      <Modal.Body className={styles.modalBody}>
+        <h3 className={`d-flex justify-content-center ${styles.alert}`}>
+          Are you sure you want to delete this menu?
+        </h3>
+        <Row>
+          <Col className="d-flex justify-content-center gap-2">
+            <Button className={styles.btnYes}>Yes</Button>
+            <Button className={styles.btnNo}>No</Button>
+          </Col>
+        </Row>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
 const MenuContent: React.FC<ContainerProps> = ({}) => {
+  const [menuModal, setMenuModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
   return (
     <div className={styles.tableContainer}>
       <div className="">
@@ -16,9 +249,16 @@ const MenuContent: React.FC<ContainerProps> = ({}) => {
               <h3>Restaurant Menu</h3>
             </Col>
           </Row>
+          {/* Button trigger modal */}
           <Row>
             <Col>
-              <Button className={styles.btnAddMenu}> Add Menu </Button>
+              <Button
+                className={styles.btnAddMenu}
+                onClick={() => setMenuModal(true)}
+              >
+                Add Menu
+              </Button>
+              <MenuModal show={menuModal} onHide={() => setMenuModal(false)} />
             </Col>
           </Row>
           <Row className="d-none d-lg-block">
@@ -30,35 +270,67 @@ const MenuContent: React.FC<ContainerProps> = ({}) => {
               />
             </Col>
           </Row>
-          <Row>
-            <Col>
-              <thead className={styles.theadHeader}>
-                <tr>
-                  <th className={styles.thHeader}>Name</th>
-                  <th className={styles.thHeader}>Description</th>
-                  <th className={styles.thHeader}>Price</th>
-                  <th className={styles.thHeader}>Availability</th>
-                  <th className={styles.thHeader}>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className={styles.tdBody}>Title of the food</td>
-                  <td className={styles.tdBody}>Food Description</td>
-                  <td className={styles.tdBody}>100 php</td>
-                  <td className={styles.tdBody}>
-                    <div>
-                      <Form.Check type="switch" />
-                    </div>
-                  </td>
-                  <td className={styles.tdBody}>
-                    <Button className={styles.btnEdit}>Edit</Button>{" "}
-                    <Button className={styles.btnDelete}>Delete</Button>
-                  </td>
-                </tr>
-              </tbody>
-            </Col>
-          </Row>
+          <Table>
+            <thead className={styles.tHeader}>
+              <tr>
+                <th>
+                  <p className={styles.textParagrap}>Name</p>
+                </th>
+                <th>
+                  <p className={styles.textParagrap}>Description</p>
+                </th>
+                <th>
+                  <p className={styles.textParagrap}>Price</p>
+                </th>
+                <th>
+                  <p className={styles.textParagrap}>Availability</p>
+                </th>
+                <th>
+                  <p className={styles.textParagrap}>Action</p>
+                </th>
+              </tr>
+            </thead>
+            <tbody className={styles.tBody}>
+              <tr>
+                <td>
+                  <p className={styles.textParagrap2}>Title of the Food</p>
+                </td>
+                <td>
+                  <p className={styles.textParagrap2}>Food Description</p>
+                </td>
+                <td>
+                  <p className={styles.textParagrap2}>100 php</p>
+                </td>
+                <td>
+                  <Form.Check type="switch" />
+                </td>
+                <td>
+                  <div>
+                    <Button
+                      className={styles.btnEdit}
+                      onClick={() => setEditModal(true)}
+                    >
+                      Edit
+                    </Button>
+                    <EditModal
+                      show={editModal}
+                      onHide={() => setEditModal(false)}
+                    />
+                    <Button
+                      className={styles.btnDelete}
+                      onClick={() => setDeleteModal(true)}
+                    >
+                      Delete
+                    </Button>
+                    <DeleteModal
+                      show={deleteModal}
+                      onHide={() => setDeleteModal(false)}
+                    />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
         </Form>
       </div>
     </div>
--- vendor/src/components/Account/Profile/ProfileContent.tsx
+++ vendor/src/components/Account/Profile/ProfileContent.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import { Table, Form, Row, Col, Button } from "react-bootstrap";
+import { Table, Form, Row, Col, Button, Modal } from "react-bootstrap";
 
 import styles from "./ProfileContent.module.scss";
 import SearchIcon from "../../../assets/images/search.png";
 
 interface ContainerProps {}
 
-const ProfileContent: React.FC<ContainerProps> = ({}) => {
+function ProfileModal(props: any) {
   return (
-    <div className={styles.profileContentContainer}>
-      <div className="">
-        <h3>Restaurant Information</h3>
+    <Modal {...props} size="lg">
+      <Modal.Header closeButton className={styles.modalHeader}></Modal.Header>
+      <Modal.Body className={styles.modalBody}>
         <Form>
           <Row lg={2} xs={1}>
             <Col>
@@ -48,47 +48,16 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
               </Form.Group>
             </Col>
           </Row>
-          <Row lg={2} xs={1}>
+          <Row>
             <Col>
               <Form.Group className="position-relative">
-                <Form.Label>Building number and Name</Form.Label>
+                <Form.Label>Resturant Address</Form.Label>
                 <Form.Control
-                  id="address1"
+                  id="address"
                   type="text"
-                  placeholder="Unit 123, GT Tower Intl."
+                  placeholder="Unit 123, GT Tower Intl., Ayala Avenue, Makati City"
                 />
               </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="position-relative">
-                <Form.Label>Street Name and Barangay</Form.Label>
-                <Form.Control
-                  id="addres2"
-                  type="text"
-                  placeholder="Ayala Avenue, Brgy Poblacion"
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row lg={2} xs={1}>
-            <Col>
-              <Form.Group className="position-relative">
-                <Form.Label>City or Town, Pincode</Form.Label>
-                <Form.Control
-                  id="address3"
-                  type="text"
-                  placeholder="Makati City, 4114"
-                />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="position-relative"></Form.Group>
-              <Form.Label>Landmark</Form.Label>
-              <Form.Control
-                id="landmark"
-                type="text"
-                placeholder="In front of RCBC Tower"
-              />
             </Col>
           </Row>
           <Row lg={2} xs={1}>
@@ -115,8 +84,110 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
           </Row>
           <Row>
             <Col>
-              <Button className={styles.btnSave}>save</Button>
-              <Button className={styles.btnEdit}>edit</Button>
+              <Button className={styles.btnUpdate}>Update</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+const ProfileContent: React.FC<ContainerProps> = ({}) => {
+  const [profileModal, setProfileModal] = useState(false);
+  return (
+    <div className={styles.profileContentContainer}>
+      <div className="">
+        <h3>Restaurant Information</h3>
+        <Form>
+          <Row lg={2} xs={1}>
+            <Col>
+              <Form.Group className="position-relative">
+                <Form.Label>Owner Name</Form.Label>
+                <Form.Control
+                  id="owner_name"
+                  type="text"
+                  placeholder="JohnDoe2022"
+                  disabled
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row lg={2} xs={1}>
+            <Col>
+              <Form.Group className="position-relative">
+                <Form.Label>Restaurant Name</Form.Label>
+                <Form.Control
+                  id="restaurant_name"
+                  type="text"
+                  placeholder="Měiwèi de shíwù 美味的食物"
+                  disabled
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="position-relative">
+                <Form.Label>Restaurant Description</Form.Label>
+                <Form.Control
+                  id="restaurant_description"
+                  as="textarea"
+                  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+                  disabled
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className="position-relative">
+                <Form.Label>Resturant Address</Form.Label>
+                <Form.Control
+                  id="address"
+                  type="text"
+                  placeholder="Unit 123, GT Tower Intl., Ayala Avenue, Makati City"
+                  disabled
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row lg={2} xs={1}>
+            <Col>
+              <Form.Group className="position-relative">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  id="email"
+                  type="text"
+                  placeholder="johndoe2022@gmail.com"
+                  disabled
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="position-relative">
+                <Form.Label>Contact Number</Form.Label>
+                <Form.Control
+                  id="contact_number"
+                  type="text"
+                  placeholder="(+63)917 456 7890"
+                  disabled
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button
+                className={styles.btnEdit}
+                onClick={() => setProfileModal(true)}
+              >
+                edit
+              </Button>
+              <ProfileModal
+                show={profileModal}
+                onHide={() => setProfileModal(false)}
+              />
             </Col>
           </Row>
         </Form>
