787511f83a5f45671637409fb9b26a944dae4065
jestoni-1bit <jestoni@1bit.com.ph>
2022-11-11 14:19:12 +0800

    Customer - Fixed resend OTP

--- customer/src/components/Otp/OtpForm.tsx
+++ customer/src/components/Otp/OtpForm.tsx
@@ -101,16 +101,7 @@ const OtpForm: React.FC<ContainerProps> = ({}) => {
       navigate("/register");
       return;
     }
-
-    otpRequestData = {
-      mobile: getMobile(),
-
-      /* Remove in production */
-      testing: true,
-    };
-
-    console.log("Requesting otp ...", otpRequestData);
-    handleSendOTP();
+    // handleSendOTP();
   }, []);
 
   // Countdown timer
@@ -120,7 +111,19 @@ const OtpForm: React.FC<ContainerProps> = ({}) => {
 
   // Send OTP request
   const handleSendOTP = async () => {
+    const otpRequestData = {
+      mobile: getMobile(),
+
+      /* Remove in production */
+      testing: true,
+    };
+
+    // Reset counter & errors
     setCounter(30);
+    setError("");
+
+    console.log("Requesting otp ...", otpRequestData);
+
     const response = await requestOTP(otpRequestData);
     console.log("handleSendOTP response", response);
     setOtpCode(response.code);
