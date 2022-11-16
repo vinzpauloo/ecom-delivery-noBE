2e4f6876a2517b0a17b4c6d01dcd79e5e9873b4b
jestoni-1bit <jestoni@1bit.com.ph>
2022-11-16 12:54:03 +0800

    Customer - Order status revisions (2)

--- customer/src/components/Order/OrderContent.tsx
+++ customer/src/components/Order/OrderContent.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+
+import statusIsReceived from "../../assets/images/order-received.png";
+import statusIsPreparing from "../../assets/images/kitchen-prep.png";
+import statusIsOtw from "../../assets/images/rider-on-the-way.png";
+import statusIsDelivered from "../../assets/images/delivered.png";
 
 import styles from "./OrderContent.module.scss";
 
@@ -8,7 +13,39 @@ interface ContainerProps {}
 const OrderContent: React.FC<ContainerProps> = ({}) => {
   return (
     <div className={styles.container}>
-      <h1>Order Content</h1>
+      <div className="text-center">
+        <div className={styles.title}>
+          <h3>Order Tracker</h3>
+          <p>Please don't close the page.</p>
+        </div>
+
+        <Row md={4} xs={1}>
+          <Col>
+            <div className={styles.status}>
+              <img src={statusIsReceived} alt="" />
+              <p>Order Received</p>
+            </div>
+          </Col>
+          <Col>
+            <div className={styles.status}>
+              <img src={statusIsPreparing} alt="" />
+              <p>Kitchen Preparing ...</p>
+            </div>
+          </Col>
+          <Col>
+            <div className={styles.status}>
+              <img src={statusIsOtw} alt="" />
+              <p>Rider on its way</p>
+            </div>
+          </Col>
+          <Col>
+            <div className={styles.status}>
+              <img src={statusIsDelivered} alt="" />
+              <p>Delivered</p>
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
