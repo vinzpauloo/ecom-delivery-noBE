2f812412f02bbcdd991ba144d21754d270af9ce6
Mark <mark@1bit.com.ph>
2022-12-02 13:03:37 +0800

    add the pin location in vendor registration

--- vendor/src/components/Registration/RegistrationForm.tsx
+++ vendor/src/components/Registration/RegistrationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Button, Form, Row, Col } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -15,6 +15,22 @@ import LogoHeader from "../../assets/images/logo-header.png";
 import LogoHeaderHover from "../../assets/images/logo-header-hover.png";
 import DefaultThumbnail from "../../assets/images/default-thumbnail.jpg";
 
+import { useGoogleAPI } from "../../hooks/useGoogleAPI";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+import { Modal } from "react-bootstrap";
+
 // Setup form schema & validation
 interface IFormInputs {
   mobile: string;
@@ -24,9 +40,17 @@ interface IFormInputs {
   last_name: string;
   email: string;
   name: string;
-  address: string;
   contact_number: string;
+  restaurant_email: string;
 }
+
+const libraries: (
+  | "places"
+  | "drawing"
+  | "geometry"
+  | "localContext"
+  | "visualization"
+)[] = ["places"];
 
 const schema = yup
   .object({
@@ -50,15 +74,100 @@ const schema = yup
     last_name: yup.string().min(2, constants.form.error.lastNameMin).required(),
     email: yup.string().email(constants.form.error.email).required(),
     name: yup.string().min(2, constants.form.error.nameMin).required(),
-    address: yup.string().required(),
     contact_number: yup
       .string()
       .matches(/^(09|\+639)\d{9}$/, constants.form.error.mobile)
       .required(),
+    restaurant_email: yup.string().email(constants.form.error.email).required(),
   })
   .required();
 
 interface ContainerProps {}
+
+const API_KEY: string = process.env.REACT_APP_GOOGLE_PLACES_API_KEY || "";
+
+const Map = ({
+  lat,
+  lng,
+  mapOnClick,
+}: {
+  lat: number;
+  lng: number;
+  mapOnClick: any;
+}) => {
+  const center = useMemo(() => ({ lat: lat, lng: lng }), [lat, lng]);
+
+  return (
+    <GoogleMap
+      zoom={18}
+      center={center}
+      mapContainerClassName={styles.map}
+      onClick={(e) => mapOnClick(e)}
+    >
+      <Marker position={center} />
+    </GoogleMap>
+  );
+};
+
+const PlacesAutocomplete = ({
+  address,
+  setAddress,
+  setLat,
+  setLng
+}: {
+  address: string;
+  setAddress: any;
+  setLat: any;
+  setLng: any;
+}) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address: any) => {
+    setValue(address, false);
+    setAddress(address);
+    clearSuggestions();
+
+    // Address to Geocode conversion
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setLat(lat);
+    setLng(lng);
+  };
+
+  useEffect(() => {
+    setValue(address, false);
+    clearSuggestions();
+  }, [address]);
+
+  return (
+    <Form.Group className="position-relative">
+      {/* <Form.Label>Full Address</Form.Label> */}
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={!ready}
+          placeholder="Address"
+          className={styles.addressInput}
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ place_id, description }) => (
+                <ComboboxOption key={place_id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </Form.Group>
+  );
+};
 
 const RegistrationForm: React.FC<ContainerProps> = ({}) => {
   const [isChecked, setIsChecked] = useState(false);
@@ -66,9 +175,20 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
   const [images, setImages] = React.useState<any>();
   const [isLoading, setIsLoading] = useState(true);
   const [defaultImg, setDefaultImg] = useState(true);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [status, setStatus] = useState("");
+  const [modalShow, setModalShow] = useState(false);
+  const [address, setAddress] = useState("");
   const maxNumber = 1;
   const navigate = useNavigate();
   const { validateFields } = useValidate();
+  const { reverseGeocode } = useGoogleAPI();
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: API_KEY,
+    libraries: libraries,
+  });
 
   const {
     register,
@@ -83,12 +203,10 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
     addUpdateIndex: number[] | undefined
   ) => {
     // data for submit
-    console.log(imageList, addUpdateIndex);
     setImages(imageList as never[]);
   };
 
   const handleClick = (onImageUpload: any) => {
-    console.log("aaaa");
     setDefaultImg((prev) => !prev);
     onImageUpload();
   };
@@ -99,10 +217,24 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
   };
 
   const onSubmit = async (data: IFormInputs) => {
-    const data1 = {...data};
+    console.log(address);
+    const data1 = {
+      ...data, 
+      address: address,
+      lat: lat.toString(),
+      long: lng.toString()
+    };
     // Validate fields
-    const data2 = { ...data, photo: images[0].photo };
+    const data2 = { 
+      ...data,
+      photo: images[0].photo,
+      address: address,
+      lat: lat.toString(),
+      long: lng.toString(),
+    };
     const response = await validateFields(data1);
+    console.log(data2);
+    
 
     if (response.errors) {
       // Prepare errors
@@ -120,12 +252,95 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
     }
   };
 
+  const handleReverseGeocode = async (lat: number, lng: number) => {
+
+    const response = await reverseGeocode(lat, lng);
+
+    setAddress(response);
+  };
+
+  const handlePinLocation = () => {
+
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+
+      navigator.permissions
+        .query({
+          name: "geolocation",
+        })
+        .then(function (result) {
+
+          if (result.state === "denied") {
+            alert(
+              "Location access is denied by your browser. Please grant location permission."
+            );
+          }
+
+          // if (result.state === "granted") setModalShow(true);
+
+          result.onchange = function () {
+
+
+            if (result.state === "denied") {
+              alert(
+                "Location access is denied by your browser. Please grant location permission."
+              );
+            }
+          };
+        });
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+
+          setStatus("");
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+          setModalShow(true);
+
+          // Reverse Geocode
+          handleReverseGeocode(
+            position.coords.latitude,
+            position.coords.longitude
+          );
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  const mapOnClick = async (e: any) => {
+
+    setLat(e.latLng.lat());
+    setLng(e.latLng.lng());
+
+    // Reverse Geocode
+    handleReverseGeocode(e.latLng.lat(), e.latLng.lng());
+  };
+
   return (
     <div>
+      <Modal
+        size="xl"
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body className="p-0">
+          <p className={`px-2 py-2 mb-0 text-center ${styles.modalAddress}`}>
+            <strong>LOCATION:</strong> {address}
+          </p>
+
+          <Map lat={lat} lng={lng} mapOnClick={mapOnClick} />
+        </Modal.Body>
+      </Modal>
       <Form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.formInnerContainer}>
           <h3 className="text-center">Owner Info</h3>
-
           <Row>
             <Col>
               <Form.Group className="position-relative">
@@ -208,23 +423,27 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
                       placeholder="Restaurant Name"
                       required
                       {...register("name")}
-                    />
+                  />
                   </Form.Group>
                 </Col>
-                <Col className="col-lg-3 col-md-4 ps-1">
-                  <Button className={styles.pinBtn}>PIN MY RESTAURANT</Button>
+                <Col className="col-lg-3 col-md-4 ps-1">             
+                  <Button
+                    variant="primary"
+                    className={styles.pin}
+                    onClick={handlePinLocation}
+                  >
+                    Pin my location
+                  </Button>
                 </Col>
               </Row>
               <Row>
                 <Col>
-                  <Form.Group className="position-relative">
-                    <Form.Control
-                      type="text"
-                      placeholder="Address"
-                      required
-                      {...register("address")}
-                    />
-                  </Form.Group>
+                  <PlacesAutocomplete
+                    address={address}
+                    setAddress={setAddress}
+                    setLat={setLat}
+                    setLng={setLng}
+                  />
                 </Col>
               </Row>
               <Row>
@@ -247,7 +466,7 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
                       type="text"
                       placeholder="Email Address"
                       required
-                      // {...register("contact_number")}
+                      {...register("restaurant_email")}
                     />
                   </Form.Group>
                 </Col>
@@ -318,8 +537,8 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
             <p>{errors.password?.message}</p>
             <p>{errors.password_confirmation?.message}</p>
             <p>{errors.name?.message}</p>
-            <p>{errors.address?.message}</p>
             <p>{errors.contact_number?.message}</p>
+            <p>{errors.restaurant_email?.message}</p>
           </div>
         </div>
 
