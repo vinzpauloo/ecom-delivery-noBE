117688ff4b928c71ed55eac2c87362991b8f3373
Mark <mark@1bit.com.ph>
2022-12-06 12:20:23 +0800

    add the pin location in update profile page

--- vendor/src/components/Account/Profile/ProfileContent.tsx
+++ vendor/src/components/Account/Profile/ProfileContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Table, Form, Row, Col, Button, Modal } from "react-bootstrap";
 import { useNavigate, Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -13,6 +13,14 @@ import SearchIcon from "../../../assets/images/search.png";
 import DefaultThumbnail from "../../../assets/images/default-thumbnail.jpg";
 import { createRefresh } from "react-auth-kit";
 
+import { useGoogleAPI } from "../../../hooks/useGoogleAPI";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import { Combobox, ComboboxInput, ComboboxList, ComboboxOption, ComboboxPopover } from "@reach/combobox";
+
 // Setup form schema & validation
 interface IFormInputs {
   first_name: string;
@@ -22,6 +30,14 @@ interface IFormInputs {
   email: string;
   contact_number: string;
 }
+
+const libraries: (
+  | "places"
+  | "drawing"
+  | "geometry"
+  | "localContext"
+  | "visualization"
+)[] = ["places"];
 
 const schema = yup
   .object({
@@ -36,10 +52,105 @@ const schema = yup
 
 interface ContainerProps {}
 
+const API_KEY: string = process.env.REACT_APP_GOOGLE_PLACES_API_KEY || "";
+
+const Map = ({
+  lat,
+  lng,
+  mapOnClick,
+}: {
+  lat: number;
+  lng: number;
+  mapOnClick: any;
+}) => {
+  const center = useMemo(() => ({ lat: lat, lng: lng }), [lat, lng]);
+
+  return (
+    <GoogleMap
+      zoom={18}
+      center={center}
+      mapContainerClassName={styles.map}
+      onClick={(e) => mapOnClick(e)}
+    >
+      <Marker position={center} />
+    </GoogleMap>
+  );
+};
+
+const PlacesAutocomplete = ({
+  address,
+  setAddress,
+  setLat,
+  setLng,
+}: {
+  address: string;
+  setAddress: any;
+  setLat: any;
+  setLng: any;
+}) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address: any) => {
+    setValue(address, false);
+    setAddress(address);
+    clearSuggestions();
+
+    // Address to Geocode conversion
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setLat(lat);
+    setLng(lng);
+  };
+
+  useEffect(() => {
+    setValue(address, false);
+    clearSuggestions();
+  }, [address]);
+
+  return (
+    <Form.Group className="position-relative">
+      <Form.Label>Full Address</Form.Label>
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          // disabled={!ready}
+          placeholder="Address"
+          className={styles.addressInput}
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ place_id, description }) => (
+                <ComboboxOption key={place_id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </Form.Group>
+  );
+};
+
 const ProfileContent: React.FC<ContainerProps> = ({}) => {
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
-  // const [disabledProfile, setDisabledProfile] = useState(true);
+  const { reverseGeocode } = useGoogleAPI();
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [address, setAddress] = useState("");
+  const [status, setStatus] = useState("");
+  const [modalShow, setModalShow] = useState(false);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: API_KEY,
+    libraries: libraries,
+  });
 
   // const handleInput = () => {
   //   setDisabledProfile(!disabledProfile);
@@ -66,8 +177,9 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
   const onSubmit = async (data: IFormInputs) => {
     console.log(data);
     console.log("Requesting updateUser ...");
+    const updatedData = {...data, address: address}
 
-    const response = await updateUser(data);
+    const response = await updateUser(updatedData);
     console.log("updateUser response", response);
 
     if (!response.error) {
@@ -95,14 +207,92 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
       photo: response.restaurant[0]?.photo || "https://placeholder.com/",
     };
     reset(defaultValues);
+    setAddress(response.restaurant[0]?.address);
   };
 
   useEffect(() => {
     handleGetUser();
   }, []);
 
+  const handleReverseGeocode = async (lat: number, lng: number) => {
+    const response = await reverseGeocode(lat, lng);
+
+    setAddress(response);
+  };
+
+  const handlePinLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+
+      navigator.permissions
+        .query({
+          name: "geolocation",
+        })
+        .then(function (result) {
+          if (result.state === "denied") {
+            alert(
+              "Location access is denied by your browser. Please grant location permission."
+            );
+          }
+
+          // if (result.state === "granted") setModalShow(true);
+
+          result.onchange = function () {
+            if (result.state === "denied") {
+              alert(
+                "Location access is denied by your browser. Please grant location permission."
+              );
+            }
+          };
+        });
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus("");
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+          setModalShow(true);
+
+          // Reverse Geocode
+          handleReverseGeocode(
+            position.coords.latitude,
+            position.coords.longitude
+          );
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  const mapOnClick = async (e: any) => {
+    setLat(e.latLng.lat());
+    setLng(e.latLng.lng());
+
+    // Reverse Geocode
+    handleReverseGeocode(e.latLng.lat(), e.latLng.lng());
+  };
+
   return (
     <div className={styles.profileContentContainer}>
+      <Modal
+        size="xl"
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body className="p-0">
+          <p className={`px-2 py-2 mb-0 text-center ${styles.modalAddress}`}>
+            <strong>LOCATION:</strong> {address}
+          </p>
+
+          <Map lat={lat} lng={lng} mapOnClick={mapOnClick} />
+        </Modal.Body>
+      </Modal>
       <div className="">
         <h3>Restaurant Information</h3>
         <Form onSubmit={handleSubmit(onSubmit)}>
@@ -144,18 +334,22 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
               </Form.Group>
             </Col>
           </Row>
-          <Row>
-            <Col>
-              <Form.Group className="position-relative">
-                <Form.Label>Resturant Address</Form.Label>
-                <Form.Control
-                  type="text"
-                  onKeyUp={() => resetMessages()}
-                  {...register("address")}
-                  required
-                />
-              </Form.Group>
-            </Col>
+          <Row  style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <PlacesAutocomplete
+              address={address}
+              setAddress={setAddress}
+              setLat={setLat}
+              setLng={setLng}
+            />
+            <Row className={styles.pinBtnContent}>
+              <Button
+                  variant="primary"
+                  className={styles.pinBtn}
+                  onClick={handlePinLocation}
+                >
+                  Pin my location
+              </Button>
+            </Row>
           </Row>
           <Row lg={2} xs={1}>
             <Col>
