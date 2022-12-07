3f9efd685fcb5f726f381a2ad2b259593438d943
VincentPaul <oo.vincentpaul@1bit.com.ph>
2022-12-07 17:49:45 +0800

    Updated Vendor ResetPassword changed alert to modal popup

--- vendor/src/components/Account/ResetPassword/ResetPasswordContent.tsx
+++ vendor/src/components/Account/ResetPassword/ResetPasswordContent.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Button, Form, Row, Col } from "react-bootstrap";
+import { Button, Form, Row, Col, Modal } from "react-bootstrap";
 import { EyeFill, EyeSlashFill, EnvelopeFill } from "react-bootstrap-icons";
 
 import { useForm } from "react-hook-form";
@@ -39,6 +39,40 @@ const EyeIcon = ({ type }: { type: string }) => {
   );
 };
 
+const ChangePasswordSuccessModal = (props: any) => {
+  return (
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Body>
+        <div className={`text-center p-4`}>
+          <Lottie animationData={saveSuccess} loop={true} />
+          <p className="mt-4" style={{ fontWeight: "400" }}>
+            Change Password Successful
+          </p>
+
+          <Link
+            to="/"
+            className={`d-inline-block mt-2`}
+            style={{
+              background: "#e6b325",
+              border: "none",
+              borderRadius: "5px",
+              color: "black",
+              fontSize: "16px",
+              fontWeight: "300",
+              width: "180px",
+              padding: "6px",
+              textDecoration: "none",
+              transition: "all 0.3s ease-in-out",
+            }}
+          >
+            Back To Home
+          </Link>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
 const ResetPasswordContent: React.FC<ContainerProps> = ({}) => {
   const [currentPassword, setCurrentPassword] = useState("password");
   const [newPassword, setNewPassword] = useState("password");
@@ -47,6 +81,7 @@ const ResetPasswordContent: React.FC<ContainerProps> = ({}) => {
   const { getUser, updatePassword } = useChangePass();
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
+  const [modalShow, setModalShow] = useState(false);
 
   const navigate = useNavigate();
 
@@ -167,10 +202,14 @@ const ResetPasswordContent: React.FC<ContainerProps> = ({}) => {
               <Button
                 className={styles.btnReset}
                 type="submit"
-                onClick={passwordChangeAlert}
+                onClick={() => setModalShow(true)}
               >
                 Reset Password
               </Button>
+              <ChangePasswordSuccessModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+              />
             </Col>
           </Row>
         </div>
