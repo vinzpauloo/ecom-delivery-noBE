7560bbd5910af86cbe78727444ce2f03f19b09d6
Mark <mark@1bit.com.ph>
2022-12-12 13:32:28 +0800

    fix the bugs in otp registration

--- vendor/src/hooks/useUser.js
+++ vendor/src/hooks/useUser.js
@@ -6,6 +6,36 @@ export const useUser = () => {
   const { calculateHash } = useCalculateHash();
   const authHeader = useAuthHeader();
 
+  const createUser = async (data) => {
+    try {
+      // START: Access register API
+      const endpoint = "api/merchant/register";
+      const options = {
+        headers: {
+          "X-Authorization": calculateHash(endpoint, data),
+        },
+        withCredentials: true,
+      };
+
+      const response = await axios.post(endpoint, data, options);
+      // END: Access register API
+
+      if (response.status === 201) {
+        const { data } = response.data;
+
+        return data;
+      }
+    } catch (err) {
+      let error;
+      if (err && err instanceof AxiosError)
+        error = "*" + err.response?.data.message;
+      else if (err && err instanceof Error) error = err.message;
+
+      console.log("Error", err);
+      return error;
+    }
+  };
+
   const getUser = async () => {
     try {
       // START: Access user API
@@ -66,5 +96,36 @@ export const useUser = () => {
     }
   };
 
-  return { getUser, updateUser };
+  const changePassword = async (data) => {
+    try {
+      // START: Access change password API
+      const endpoint = "api/change-password";
+      const options = {
+        headers: {
+          Authorization: authHeader(),
+          "X-Authorization": calculateHash(endpoint, data),
+        },
+      };
+
+      const response = await axios.put(endpoint, data, options);
+      // END: Access change password API
+
+      if (response.status === 201) {
+        console.log(response);
+        const { data, message } = response.data;
+
+        return { token: data.token, message };
+      }
+    } catch (err) {
+      let error;
+      if (err && err instanceof AxiosError)
+        error = "*" + err.response?.data.message;
+      else if (err && err instanceof Error) error = err.message;
+
+      console.log("Error", err);
+      return { error: error };
+    }
+  };
+
+  return { createUser, getUser, updateUser, changePassword };
 };
