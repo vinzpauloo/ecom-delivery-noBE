b517005aa37f41eee81ce9544b45b82beed43791
Mark <mark@1bit.com.ph>
2022-12-15 18:39:28 +0800

    add navigation in mobile

--- vendor/src/components/Account/RestaurantFeedback/RestaurantFeedbackContainer.tsx
+++ vendor/src/components/Account/RestaurantFeedback/RestaurantFeedbackContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import NavigationContainer from "../Navigation/NavigationContainer";
 
 import styles from "./RestaurantFeedbackContainer.module.scss";
 import RestaurantFeedbackContent from "./RestaurantFeedbackContent";
@@ -8,8 +9,19 @@ interface ContainerProps {}
 
 const RestaurantFeedbackContainer: React.FC<ContainerProps> = ({}) => {
   return (
-    <Container fluid="xxl" className={styles.container}>
-      <RestaurantFeedbackContent />
+    <Container fluid="xxl">
+      <Row className={styles.container}>
+        <Col className={`${styles.mobileNav} d-lg-none`}>
+          <div className={styles.navigationContainer}>
+            <NavigationContainer />
+          </div>
+        </Col>
+        <Col>
+          <div className={styles.contentContainer}>
+            <RestaurantFeedbackContent />
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
