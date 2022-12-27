af2ea56bd3f75bf063424827badf96a112469fdb
jestoni-1bit <jestoni@1bit.com.ph>
2022-12-27 17:00:10 +0800

    Vendor - Update pin my location flow

--- vendor/src/components/Account/Profile/ProfileContent.tsx
+++ vendor/src/components/Account/Profile/ProfileContent.tsx
@@ -65,6 +65,10 @@ const schema = yup
 interface ContainerProps {}
 
 const API_KEY: string = process.env.REACT_APP_GOOGLE_PLACES_API_KEY || "";
+const DEFAULT_COORDINATES = {
+  lat: 9.568885793195934,
+  lng: 123.77310991287231,
+};
 
 const Map = ({
   lat,
@@ -79,7 +83,7 @@ const Map = ({
 
   return (
     <GoogleMap
-      zoom={18}
+      zoom={16}
       center={center}
       mapContainerClassName={styles.map}
       onClick={(e) => mapOnClick(e)}
@@ -166,8 +170,9 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
   const { reverseGeocode } = useGoogleAPI();
-  const [lat, setLat] = useState(0);
-  const [lng, setLng] = useState(0);
+  const [isGranted, setIsGranted] = useState(false);
+  const [lat, setLat] = useState(DEFAULT_COORDINATES.lat);
+  const [lng, setLng] = useState(DEFAULT_COORDINATES.lng);
   const [address, setAddress] = useState("");
   const [status, setStatus] = useState("");
   const [modalShow, setModalShow] = useState(false);
@@ -275,27 +280,35 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
     if (!navigator.geolocation) {
       setStatus("Geolocation is not supported by your browser");
     } else {
-      setStatus("Locating...");
+      setStatus("Requesting location access ...");
 
       navigator.permissions
         .query({
           name: "geolocation",
         })
         .then(function (result) {
-          if (result.state === "denied") {
-            alert(
-              "Location access is denied by your browser. Please grant location permission."
-            );
+          if (result.state === "prompt") {
+            setModalShow(true);
           }
 
-          // if (result.state === "granted") setModalShow(true);
+          if (result.state === "denied") {
+            alert(
+              "Location permission is not granted by your browser or device."
+            );
+            setModalShow(true);
+          }
+
+          if (result.state === "granted") setIsGranted(true);
 
           result.onchange = function () {
             if (result.state === "denied") {
               alert(
-                "Location access is denied by your browser. Please grant location permission."
+                "Location permission is not granted by your browser or device."
               );
+              setModalShow(true);
             }
+
+            if (result.state === "granted") setIsGranted(true);
           };
         });
 
@@ -313,7 +326,7 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
           );
         },
         () => {
-          setStatus("Unable to retrieve your location");
+          setStatus("Unable to retrieve your location.");
         }
       );
     }
@@ -376,11 +389,38 @@ const ProfileContent: React.FC<ContainerProps> = ({}) => {
         centered
       >
         <Modal.Body className="p-0">
-          <p className={`px-2 py-2 mb-0 text-center ${styles.modalAddress}`}>
-            <strong>LOCATION:</strong> {address}
-          </p>
+          {/* Old flow, need */}
+          {/* {!isGranted ? (
+            <div className="text-center py-5">
+              <p>Waiting for location permission.</p>
+              <div className="spinner-grow text-primary" role="status"></div>
+            </div>
+          ) : (
+            <>
+              <p
+                className={`px-2 py-2 mb-0 text-center ${styles.modalAddress}`}
+              >
+                <strong>LOCATION:</strong> {address}
+              </p>
+              <Map lat={lat} lng={lng} mapOnClick={mapOnClick} />
+            </>
+          )} */}
 
-          <Map lat={lat} lng={lng} mapOnClick={mapOnClick} />
+          {/* Revised flow, show default Google Map coordinates */}
+          <>
+            <p className={`px-2 py-2 mb-0 text-left ${styles.modalAddress}`}>
+              {isGranted ? (
+                <>
+                  <strong>LOCATION:</strong> {address}
+                </>
+              ) : (
+                <>
+                  <strong>WARNING:</strong> {status}
+                </>
+              )}
+            </p>
+            <Map lat={lat} lng={lng} mapOnClick={mapOnClick} />
+          </>
         </Modal.Body>
       </Modal>
       <div className={styles.mainContainer}>
--- vendor/src/components/Registration/RegistrationForm.tsx
+++ vendor/src/components/Registration/RegistrationForm.tsx
@@ -85,6 +85,10 @@ const schema = yup
 interface ContainerProps {}
 
 const API_KEY: string = process.env.REACT_APP_GOOGLE_PLACES_API_KEY || "";
+const DEFAULT_COORDINATES = {
+  lat: 9.568885793195934,
+  lng: 123.77310991287231,
+};
 
 const Map = ({
   lat,
@@ -99,7 +103,7 @@ const Map = ({
 
   return (
     <GoogleMap
-      zoom={18}
+      zoom={16}
       center={center}
       mapContainerClassName={styles.map}
       onClick={(e) => mapOnClick(e)}
@@ -175,8 +179,9 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
   const [images, setImages] = React.useState<any>();
   const [isLoading, setIsLoading] = useState(true);
   const [defaultImg, setDefaultImg] = useState(true);
-  const [lat, setLat] = useState(0);
-  const [lng, setLng] = useState(0);
+  const [isGranted, setIsGranted] = useState(false);
+  const [lat, setLat] = useState(DEFAULT_COORDINATES.lat);
+  const [lng, setLng] = useState(DEFAULT_COORDINATES.lng);
   const [status, setStatus] = useState("");
   const [modalShow, setModalShow] = useState(false);
   const [address, setAddress] = useState("");
@@ -271,27 +276,35 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
     if (!navigator.geolocation) {
       setStatus("Geolocation is not supported by your browser");
     } else {
-      setStatus("Locating...");
+      setStatus("Requesting location access ...");
 
       navigator.permissions
         .query({
           name: "geolocation",
         })
         .then(function (result) {
-          if (result.state === "denied") {
-            alert(
-              "Location access is denied by your browser. Please grant location permission."
-            );
+          if (result.state === "prompt") {
+            setModalShow(true);
           }
 
-          // if (result.state === "granted") setModalShow(true);
+          if (result.state === "denied") {
+            alert(
+              "Location permission is not granted by your browser or device."
+            );
+            setModalShow(true);
+          }
+
+          if (result.state === "granted") setIsGranted(true);
 
           result.onchange = function () {
             if (result.state === "denied") {
               alert(
-                "Location access is denied by your browser. Please grant location permission."
+                "Location permission is not granted by your browser or device."
               );
+              setModalShow(true);
             }
+
+            if (result.state === "granted") setIsGranted(true);
           };
         });
 
@@ -309,7 +322,7 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
           );
         },
         () => {
-          setStatus("Unable to retrieve your location");
+          setStatus("Unable to retrieve your location.");
         }
       );
     }
@@ -333,11 +346,38 @@ const RegistrationForm: React.FC<ContainerProps> = ({}) => {
         centered
       >
         <Modal.Body className="p-0">
-          <p className={`px-2 py-2 mb-0 text-center ${styles.modalAddress}`}>
-            <strong>LOCATION:</strong> {address}
-          </p>
+          {/* Old flow, need */}
+          {/* {!isGranted ? (
+            <div className="text-center py-5">
+              <p>Waiting for location permission.</p>
+              <div className="spinner-grow text-primary" role="status"></div>
+            </div>
+          ) : (
+            <>
+              <p
+                className={`px-2 py-2 mb-0 text-center ${styles.modalAddress}`}
+              >
+                <strong>LOCATION:</strong> {address}
+              </p>
+              <Map lat={lat} lng={lng} mapOnClick={mapOnClick} />
+            </>
+          )} */}
 
-          <Map lat={lat} lng={lng} mapOnClick={mapOnClick} />
+          {/* Revised flow, show default Google Map coordinates */}
+          <>
+            <p className={`px-2 py-2 mb-0 text-left ${styles.modalAddress}`}>
+              {isGranted ? (
+                <>
+                  <strong>LOCATION:</strong> {address}
+                </>
+              ) : (
+                <>
+                  <strong>WARNING:</strong> {status}
+                </>
+              )}
+            </p>
+            <Map lat={lat} lng={lng} mapOnClick={mapOnClick} />
+          </>
         </Modal.Body>
       </Modal>
       <Form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
